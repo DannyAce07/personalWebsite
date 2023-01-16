8beeb4bde96286230ef82f4a83a8b7eb7915035d
Ace <120422283+DannyAce07@users.noreply.github.com>
2023-01-16 12:53:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ if (confirm("Would you like to know the average income for Web Developers across
   document.write("That's too bad ", userName);
   }
 
-const countEl = document.getElementById('count');
+const countEl = document.getElementById('counter');
 
-function updateCount() {
+function updateCounter() {
   fetch('https://api.countapi.xyz/update/dannyace/github/?amount=1')
     .then(res => res.json())
     .then(res => {
